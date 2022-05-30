42dc22905701c4b04caa32f79295c6667762c0c3
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-05-30 22:14:11 +0300

    fix: change tag selectors to class rules  3.1 3.2 3.3

--- app.js
+++ app.js
@@ -32,13 +32,15 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "list__item";
     label.innerText=taskString;
-    label.className='task';
+    label.className="label-task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox"
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-task hidden-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="btn edit";
